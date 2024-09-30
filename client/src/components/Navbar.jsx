92b76d73b1ea2baf9a96dcbb0bffe45fd1b15e79
developerzubi <zubairsoftwaredeveloper@gmail.com>
2024-09-29 21:43:51 -0700

    navbar edit

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,20 +1,21 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
     <div className='absolute top-0 left-0 w-full p-4'>
   <div className='flex justify-between items-center'>
-    <h1 className='text-white text-2xl font-bold'>Logo</h1>
+    <h1 className='text-white text-2xl font-bold'><Link to={'/'}>PDf & Photo Editor</Link></h1>
     <nav>
       <ul className='flex space-x-8'>
         <li className='text-white text-lg font-semibold px-4 py-2 rounded-md hover:bg-gradient-to-r from-blue-400 to-cyan-500 transition-all duration-300 cursor-pointer'>
-          Home
+          <Link to={'/'}>Home</Link>
         </li>
         <li className='text-white text-lg font-semibold px-4 py-2 rounded-md hover:bg-gradient-to-r from-blue-400 to-cyan-500 transition-all duration-300 cursor-pointer'>
-          Register
+        <Link to={'/signup'}>Register</Link>
         </li>
         <li className='text-white text-lg font-semibold px-4 py-2 rounded-md hover:bg-gradient-to-r from-blue-400 to-cyan-500 transition-all duration-300 cursor-pointer'>
-          Signin
+        <Link to={'/login'}>Signin</Link>
         </li>
       </ul>
     </nav>
